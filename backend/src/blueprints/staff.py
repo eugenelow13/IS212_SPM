fc15e04f7c06363a8478709190d98aa9ed8cffd2
Ng Yin Kit <yinkit.ng.2021@scis.smu.edu.sg>
2023-09-24 00:49:04 +0800

    deleted staff.py right under src, minor changes to staff.py under blueprints

--- backend/src/blueprints/staff.py
+++ backend/src/blueprints/staff.py
@@ -5,18 +5,16 @@ from src.models import Staff
 staff = Blueprint("staff", __name__, url_prefix="/staff")
 
 @staff.route("/", methods=["GET"])
-def see_staffs():
-    stafflist = Staff.query.all()
-    if(len(stafflist)):
-        return jsonify({"staff": [Staff.json() for Staff in stafflist]})
-    return jsonify({"message": "No staffs found."}), 404
+def get_all():
+    staff_list = Staff.query.all()
+    if(len(staff_list)):
+        return jsonify({"staff": [staff.json() for staff in staff_list]})
+    return jsonify({"message": "No staff found."}), 404
 
 @staff.route("/<int:staff_id>", methods=["GET"])
-def see_staff():
+def get_staff():
     pass
 
 @staff.route("/", methods=["POST"])
 def create_staff():
     pass
-
-
