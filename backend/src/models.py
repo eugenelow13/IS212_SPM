3f318264b0799d2a5e04faf6d683aa8959813384
Eugene <eugene.low.2021@scis.smu.edu.sg>
2023-09-23 22:54:58 +0800

    SCRUM-36 Updated models.py

--- backend/src/models.py
+++ backend/src/models.py
@@ -4,20 +4,22 @@ from typing import List
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.orm import relationship
 
+
+
 class Staff(db.Model):
     __tablename__ = 'staff'
-    staffid = db.Column(db.Integer, primary_key = True, auto_increment=True)
-    stafffname = db.Column(db.String(50), nullable=False)
-    stafflname = db.Column(db.String(50), nullable=False)
+    staff_id = db.Column(db.Integer, primary_key= True)
+    staff_fname = db.Column(db.String(50), nullable=False)
+    staff_lname = db.Column(db.String(50), nullable=False)
     dept = db.Column(db.String(50), nullable=False)
     country = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
-    access_role = db.Column(db.Integer, nullable=False, foreignkey=True)
+    access_role = db.Column(db.Integer, nullable=False)
+    
 
-
-    def __init__(self,stafffname, stafflname, dept, country, email, access_role):
-        self.stafffname = stafffname
-        self.stafflname = stafflname
+    def __init__(self,staff_fname, staff_lname, dept, country, email, access_role):
+        self.staff_fname = staff_fname
+        self.staff_lname = staff_lname
         self.dept = dept
         self.country = country
         self.email = email
@@ -25,9 +27,9 @@ class Staff(db.Model):
 
 
     def json(self):
-        return{"Staff_ID": self.staffid,
-               "Staff_FName": self.stafffname,
-               "Staff_LName": self.stafflname,
+        return{"Staff_ID": self.staff_id,
+               "Staff_FName": self.staff_fname,
+               "Staff_LName": self.staff_lname,
                "Dept": self.dept,
                "Country": self.country,
                "Email": self.email,
