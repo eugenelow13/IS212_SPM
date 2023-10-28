db09fbb6b3088ab949100cd4408cb9b11ba37a01
seanl01 <57535001+seanl01@users.noreply.github.com>
2023-10-29 00:51:35 +0800

    Test for manager select

--- sbrp_client/cypress/e2e/managerSelect.cy.js
+++ sbrp_client/cypress/e2e/managerSelect.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="Cypress" />
 
+const managers = require("../fixtures/managers.json").staffs
+
 describe("test manager select", () => {
     beforeEach(() => {
         cy.login();
@@ -7,6 +9,17 @@ describe("test manager select", () => {
 
     it("should open and see list of managers", () => {
         cy.visit("/listings/new");
-        cy.get(".select__control").click();
-        
+        cy.get(".select__control")
+            .contains("name or staff")
+            .click({force: true});
+            
+        cy.get(".select__menu-list").as("manager-list")
+            .children()
+            .should("have.length", managers.length)
+
+        managers.forEach((manager) => {
+            cy.get("@manager-list")
+            .should("contain", manager.staff_id)
+        })
     })
+})
