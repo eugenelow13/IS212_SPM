e4e19028f76da34691c84ca3d60ba6beac9190b3
seanl01 <57535001+seanl01@users.noreply.github.com>
2023-10-29 01:54:51 +0800

    Test case for self-description

--- sbrp_client/cypress/e2e/viewApplicant.cy.js
+++ sbrp_client/cypress/e2e/viewApplicant.cy.js
@@ -11,14 +11,20 @@ describe('test applicant details', () => {
 
     const application = require('../fixtures/applications_detail.json')
 
-    it('should see applicant details', () => {
+    it('should see applicant details (SCRUM 73-1)', () => {
         // visit the applicant page
         cy
-        .visit('/applications/3')
-        .contains(application.staff_name)
-        
+            .visit('/applications/3')
+            .contains(application.staff_name)
+
         cy
-        .contains(application.staff_id)
+            .contains(application.staff_id)
+
+        cy
+            .contains(application.applicant.dept)
+
+        cy
+            .contains(application.applicant.country)
 
     })
     // find accordion component and click on it
@@ -26,8 +32,8 @@ describe('test applicant details', () => {
         cy.visit('/applications/3')
         // get first accordion item bootstrap
         cy
-        .get('.accordion-item').first().click({force: true})
-        
+            .get('.accordion-item').first().click({ force: true })
+
         cy.contains(application.role_listing.role_name)
         cy.contains(application.role_listing.country)
         cy.contains(application.role_listing.manager_name)
@@ -35,12 +41,22 @@ describe('test applicant details', () => {
 
     })
 
-    
-    it('should show self-description', () => {
+
+    it('should show self-description (SCRUM-75)', () => {
         cy.visit('/applications/3')
-        cy
-        .contains(application.app_desc.slice(0, 100))
+        cy.get('.border.rounded-2.p-2')
+            .should("contain.text", application.app_desc)
     })
+
+    // it('should show be able to scroll when description is long', () => {
+    //     cy.visit('/applications/3')
+    //     cy.get('.modal-body > .border').last()
+    //         // scroll to bottom of overflow text content in div
+    //         .scrollTo('bottom')
+
+    //         .should("contain.text", application.app_desc)
+
+    // })
 
     const roleSkills = application.role_listing.role_skills
     const staffSkills = application.applicant.staff_skills
